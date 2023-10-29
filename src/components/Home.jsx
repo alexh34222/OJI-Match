b220d1c9dec7b724bd7efc6204ff891a063d6548
alex h <alexander.hallpnw2@icloud.com>
2023-10-28 22:35:27 -0700

    created array images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,32 @@
 import "../assets/css/Home.css";
 
+import Facebook from "../assets/images/facebook.png";
+import Instagram from "../assets/images/instagram.png";
+import Twitter from "../assets/images/twitter.png";
+import Youtube from "../assets/images/youtube.png";
+import Linkedin from "../assets/images/linkedin.png";
+
 export default function Home() {
+  const cardImages = [
+    { Facebook },
+    { Instagram },
+    { Twitter },
+    { Youtube },
+    { Linkedin },
+  ];
+
   return (
-    <div className="body">
-      <h1>Memory Match Mania</h1>
-      <button>Start Game</button>
+    <div className="home">
+      <div className="homeContent">
+        {" "}
+        <div className="homeTitle">
+          <h1>Memory Match Mania</h1>
+        </div>
+        <div className="startButton">
+          {" "}
+          <button>Start Game</button>
+        </div>
+      </div>
     </div>
   );
 }
