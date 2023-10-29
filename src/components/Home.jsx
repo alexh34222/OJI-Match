638cb46d1cae95b50c20eea09d6f60dcd544c98a
alex h <alexander.hallpnw2@icloud.com>
2023-10-28 22:13:00 -0700

    game

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import "../assets/css/Home.css";
+
+export default function Home() {
+  return (
+    <div className="body">
+      <h1>Memory Match Mania</h1>
+      <button>Start Game</button>
+    </div>
+  );
+}
